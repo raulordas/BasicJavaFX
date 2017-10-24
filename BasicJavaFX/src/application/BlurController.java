7003168af2c93ed1cacdc6f455e47fa9632cbcd0
Ernesto Ramiro <atomms@gmail.com>
2017-10-24 20:35:13 +0200

    branches clip go and master

--- BasicJavaFX/src/application/BlurController.java
+++ BasicJavaFX/src/application/BlurController.java
@@ -1,17 +1,46 @@
 package application;
 
 import javafx.fxml.FXML;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.effect.GaussianBlur;
 import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Ellipse;
 
 public class BlurController {
 	
 	@FXML
-    private ImageView imageView;       
+    private ImageView imageView;   
+	private Ellipse ellipse;
 
 /**
 * inicializa al igual que el constructor pero permite acceder al componente imageView ya creado en fxml
 */
+	
+	
+	@FXML
+    public void setclip() {           
+        ellipse = new Ellipse();
+//        circle = new Circle((imageView.getFitWidth()));
+
+        ellipse.centerXProperty().setValue(95);
+        ellipse.centerYProperty().setValue(70);
+        ellipse.radiusXProperty().setValue(70);
+        ellipse.radiusYProperty().setValue(70);
+        
+//        importing and modifying sizes
+//    	ellipse.radiusXProperty().setValue(imageView.getFitWidth()*0.1);
+//    	ellipse.radiusYProperty().setValue(imageView.getFitHeight()*0.17);
+//      ellipse.radiusXProperty().bind(root.widthProperty().multiply(0.8));
+//      ellipse.radiusYProperty().bind(root.heightProperty().multiply(0.6));
+        
+        imageView.setClip(ellipse);
+
+//apply a shadow effect.
+        imageView.setEffect(new DropShadow(120, Color.BLACK));
+
+    }
+
 	@FXML
     public void setblur() {           
              imageView.setEffect(new GaussianBlur(10));
