166c63291f9a46cf5faa46767c8b43e208df0c7d
Ernesto Ramiro <atomms@gmail.com>
2018-10-02 17:15:12 +0200

    cleaned and docced

--- BasicJavaFX/src/application/Main.java
+++ BasicJavaFX/src/application/Main.java
@@ -1,5 +1,5 @@
 package application;
-	
+
 import java.io.IOException;
 
 import javafx.application.Application;
@@ -8,68 +8,52 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.effect.GaussianBlur;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
-
 
 public class Main extends Application {
 
-	private Pane mypane;
-	
-	/**
-	 * modifica el método start de la clase principal
-	 */
-	
-    @Override
-    public void start(Stage stage) throws IOException {
-    
-    	/**
-    	 * carga la vista fxml y opcionalmente un controlador para inicializar algún componente
-    	 * sobre el panel mypane colocamos la vista   
-    	 */
-    	
-    	FXMLLoader loader = new FXMLLoader(
-            getClass().getResource(
-                "../view/forest.fxml"
-            )
-        );
-    	loader.setController(new SetBlur());
-        mypane = (Pane) loader.load();
+	private AnchorPane mypane;
 
-        /**
-         * titula el stage y añade la escena al stage
-         */
-        stage.setTitle("Where's the squirrel?");
-        stage.setScene(new Scene(mypane));
-        stage.show();
-    }
-   
-    /**
-     * la clase controladora que añade un efecto a la imagen
-     */
-    class SetBlur {
-    	
-    	@FXML
-        private ImageView imageView;       
+	// modifica el método start de la clase principal
+	@Override
+	public void start(Stage stage) throws IOException {
 
-/**
- * la etiqueta FXML permite acceder al componente imageView ya creado en fxml
- */
-    	@FXML
-        public void initialize() {           
-                 imageView.setEffect(new GaussianBlur(5));
-        }
-    	
-    }
-   
-    /**
-     * arranca la aplicación
-     * @param args
-     */
-    
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
+		// primero cargamos la vista fxml en el FXMLLoader
+		FXMLLoader loader = new FXMLLoader(getClass().getResource(
+				"../view/forest.fxml"));
+
+		// y opcionalmente un controlador para inicializar algún componente
+		loader.setController(new SetBlur());
+
+		// ahora sobre el panel mypane cargamos la vista que dejamos en el
+		// FXMLLoader
+		mypane = (AnchorPane) loader.load();
+		
+		// añade la escena al stage y la titula
+		stage.setTitle("Where's the squirrel?");
+		stage.setScene(new Scene(mypane));
+		stage.show();
+	}
+
+	// la clase controladora que añade un efecto a la imagen
+	class SetBlur {
+
+		// la etiqueta FXML permite acceder al componente imageView ya creado en
+		// fxml
+		@FXML
+		private ImageView forestview;
+
+		@FXML
+		public void initialize() {
+			forestview.setEffect(new GaussianBlur(2));
+		}
+
+	}
+
+	// arranca la aplicación
+	public static void main(String[] args) {
+		launch(args);
+	}
+
 }
