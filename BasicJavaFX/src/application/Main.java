944010e51a148ece2d5504b046c70905655ae8bc
Ernesto Ramiro <atomms@gmail.com>
2018-10-03 20:01:13 +0200

    external controller

--- BasicJavaFX/src/application/Main.java
+++ BasicJavaFX/src/application/Main.java
@@ -3,11 +3,8 @@ package application;
 import java.io.IOException;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.effect.GaussianBlur;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -23,7 +20,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
     
     	/**
-    	 * carga el controlador y la vista fxml 
+    	 * carga la vista fxml 
     	 */
     	
     	FXMLLoader loader = new FXMLLoader(
@@ -31,7 +28,6 @@ public class Main extends Application {
                 "forest.fxml"
             )
         );
-//    	loader.setController(new SetBlur());
         mypane = (AnchorPane) loader.load();
 
         /**
@@ -41,31 +37,8 @@ public class Main extends Application {
         stage.setScene(new Scene(mypane));
         stage.show();
     }
-   
-
-    	
-    /**
-     * la clase controladora que añade un efecto a la imagen
-     */
-    private class SetBlur {
-    	
-    	@FXML
-        private ImageView imageView;       
-
-/**
- * inicializa al igual que el constructor pero permite acceder al componente imageView ya creado en fxml
- */
-    	@FXML
-        public void initialize() {           
-                 imageView.setEffect(new GaussianBlur(10));
-        }
-    	
-    }
-   
-
+  
     
-
-
     /**
      * arranca la aplicación
      * @param args
